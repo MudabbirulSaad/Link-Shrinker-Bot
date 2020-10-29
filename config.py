dec0d826c6769626164a49c1f7ecd308c454a1fe
Mudabbirul Islam <Mudabbirulsaad@gmail.com>
2020-10-29 20:27:18 +0600

    Now welcome & help message can be customizable + added config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config(object):
-	SHORTNER_API = "493af2f9e09e78629563729b2b911165ca4f5eef"
-	SHORTNER_URL = "https://afly.in/api?api={}&url={}&format=json"
-	BOT_TOKEN = "1258352281:AAFtUpJXzbjxi7S9TpLoC2qZ7e9ExjqS0w4"
+	SHORTNER_API = "Goes Here"
+	SHORTNER_URL = "Goes Here"
+	BOT_TOKEN = "Goes Here"
 
 class Text(object):
-	WELCOME_TEXT = "Hi, I'm a Link Shortner bot currently using adfly API !\nYou can short link using me.\nJust sent me your long URL"
+	WELCOME_TEXT = "Hi, I'm a Link Shortner bot currently using shortner API !\nYou can short link using me.\nJust sent me your long URL"
 	HELP_TEXT = "Sent me any link!\n\n\nEnample:\nhttps://google.com\nhttps://bing.com"
