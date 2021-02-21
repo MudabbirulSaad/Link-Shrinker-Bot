32c9b6f638ad4ec4ed3c97e3e73de271aadd81cc
David T Eady <61388628+DevTeady@users.noreply.github.com>
2021-02-21 14:07:02 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config(object):
-	SHORTNER_API = "Goes here"
-	SHORTNER_URL = "https://yourdomain.com/api?api={}&url={}&format=json"
-	BOT_TOKEN = "Goes here"
+	SHORTNER_API = "484c105293b3fa61508b7a81d3269d62b6b1cafc"
+	SHORTNER_URL = "https://gplinks.in/api?api={}&url={}&format=json"
+	BOT_TOKEN = "1258352281:AAFtUpJXzbjxi7S9TpLoC2qZ7e9ExjqS0w4"
 
 class Text(object):
-	WELCOME_TEXT = "*Hi, I'm a Link Shortner bot currently using shortner API !*\n*You can short link using me.*\n_Just sent me your long URL_"
+	WELCOME_TEXT = "*Hi, I'm a Link Shortner bot currently using shortner API !*\n*You can short link using me.*\n*Just sent me your long URL*"
 	HELP_TEXT = "Sent me any link!\n\nEnample:\n`https://google.com`\n`https://bing.com`"
-	MAINTAINER = "@Username"
+	MAINTAINER = "@DarkQuillium"
