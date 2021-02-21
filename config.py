8efc4c65cfc74ab508aba1120b9da8e8bc5b3459
Mudabbirul Saad <mudabbirulsaad@gmail.com>
2021-02-21 14:52:36 +0600

    dC

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
 
 class Text(object):
 	WELCOME_TEXT = "*Hi, I'm a Link Shortner bot currently using shortner API !*\n*You can short link using me.*\n_Just sent me your long URL_"
-	HELP_TEXT = "Sent me any link!\n\nEnample:\n`https://google.com`\n`https://bing.com`"
+	HELP_TEXT = "*Sent me any link!*\n\n*Enample:*\n`https://google.com`\n`https://bing.com`"
 	MAINTAINER = "@Username"
